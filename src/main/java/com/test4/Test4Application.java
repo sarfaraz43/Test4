554f303dbbfda6e55d81f42af5bee1af95e9813c
saffu@gmail.com <saffu@gmail.com>
2026-09-16 12:36:12 +0530

    3

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -9,6 +9,7 @@ public class Test4Application {
 	public static void main(String[] args) {
                     int x  = 100;
                     int y =200;
+                     booean isSub = true;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
